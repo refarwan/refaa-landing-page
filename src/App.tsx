89f1116b4cc41ad20e15ec0dd4c1b4f0e742d774
Aan Refarwan <refarwan@gmail.com>
2024-05-23 17:31:31 +0700

    feat: add about component

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,29 @@ function App() {
 				alt="Random Blob"
 				className="w-[558px] absolute top-[425px] left-[70px] -z-50 sm:top-[312px] sm:left-[395px] xl:left-[unset] xl:-right-[160px] xl:top-[251px]"
 			/>
+			<div
+				id="about"
+				className="bg-white/80 flex flex-col gap-[15px] px-[20px] py-[10px] items-center my-[30px] sm:p-[20px] sm:px-[70px] xl:w-[1200px] xl:mx-auto xl:bg-transparent xl:py-[60px] xl:px-[100px]"
+			>
+				<h2 className="font-semibold text-[20px]">ABOUT</h2>
+				<p className="text-center text-gray-500">
+					Di Refaa Technology Solutions, kami berdedikasi untuk membantu bisnis
+					Anda mencapai potensi penuh melalui solusi ERP yang inovatif dan
+					terintegrasi. Berawal dari visi untuk menghadirkan teknologi canggih
+					yang mudah diakses dan digunakan oleh berbagai jenis usaha, kami telah
+					berkembang menjadi mitra strategis bagi banyak perusahaan dalam
+					mengoptimalkan operasional mereka. Tim kami terdiri dari para ahli
+					teknologi berpengalaman yang berkomitmen untuk memberikan layanan
+					terbaik dan solusi yang disesuaikan dengan kebutuhan spesifik bisnis
+					Anda. Dengan pendekatan yang mengutamakan pelanggan, kami memastikan
+					setiap solusi ERP yang kami kembangkan mampu meningkatkan efisiensi,
+					produktivitas, dan daya saing perusahaan Anda. Refaa Technology
+					Solutions berkomitmen untuk terus berinovasi dan memberikan nilai
+					tambah melalui produk dan layanan kami. Bersama kami, wujudkan
+					transformasi digital yang efektif dan berkelanjutan untuk masa depan
+					bisnis yang lebih cerah
+				</p>
+			</div>
 		</>
 	);
 }
